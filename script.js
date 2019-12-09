4637b5efc2dd414931b44b52c146a24e39035b9d
P W <przemo2048@gmail.com>
2019-12-09 15:43:09 +0100

    dodanie postępów

---
+++ script.js
@@ -0,0 +1,27 @@
+const menuButton = document.querySelector('.page-navigation_menuIcon');
+
+menuButton.addEventListener('click', event => {
+	console.log('clicked');
+	let menu = document.querySelector('.page-navigation__items');
+	menu.classList.toggle('toggled');
+});
+
+const ticketButtons = document.querySelectorAll('.concerts__info .button');
+
+ticketButtons.forEach(btn => {
+
+	 btn.addEventListener('click', event => {
+		// console.log( event.target.id );
+		btn.style.visibility = 'hidden';
+	 });
+});
+
+
+const contactForm = document.querySelector('.contact__form');
+
+contactForm.addEventListener('submit', function(event) {
+	event.preventDefault();
+	const formData = new FormData(this);
+	const entires = formData.entries();
+	console.log(Object.fromEntries(entires));
+});
