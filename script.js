6047791fda67b212accf7e0b593626e68b9f2919
P W <przemo2048@gmail.com>
2019-12-16 15:51:09 +0100

    test

--- script.js
+++ script.js
@@ -3,40 +3,49 @@
 const menuIcon = document.querySelector('.nav-icon3');
 
 menuIcon.addEventListener('click', (e) => {
-    const pageNavigation = document.querySelector('.page-navigation');
-    pageNavigation.classList.toggle('page-navigation--visible');
-    menuIcon.classList.toggle('nav-icon3--open');
+	const pageNavigation = document.querySelector('.page-navigation');
+	pageNavigation.classList.toggle('page-navigation--visible');
+	menuIcon.classList.toggle('nav-icon3--open');
 });
 
-// menuButton.addEventListener('click', (e) => {
-//     const pageNavigation = document.querySelector('.page-navigation');
-//     pageNavigation.classList.toggle('page-navigation--visible');
-//     menuIcon.toggleClass('open');
-// });
+const galleryImages = document.querySelectorAll(".gallery__image");
+const enter = "mouseenter";
+const leave = "mouseleave";
+
+galleryImages.forEach((image) => {
+	image.addEventListener(enter, function(){
+	  	this.classList.remove(leave);
+	  	this.classList.add(enter);
+	  	this.addEventListener(leave, function(){
+		    this.classList.remove(enter);
+		  	this.classList.add(leave);
+		});  
+	});
+});
 
 // Mark ticket as bought
 const concerts = document.querySelector('#concerts');
 
 concerts.addEventListener('click', (e) => {
-    if (e.target.classList.contains('button--buy-ticket')) {
-        const buyTicketButton = e.target;
-        const haveFunText = document.createElement('p');
-        haveFunText.textContent = 'Have fun!';
+	if (e.target.classList.contains('button--buy-ticket')) {
+		const buyTicketButton = e.target;
+		const haveFunText = document.createElement('p');
+		haveFunText.textContent = 'Have fun!';
 
-        const buttonContainer = buyTicketButton.parentElement;
-        buttonContainer.insertBefore(haveFunText, buyTicketButton);
-        buyTicketButton.remove();
-        // OR
-        // buyTicketButton.replaceWith(haveFunText);
-    }
+		const buttonContainer = buyTicketButton.parentElement;
+		buttonContainer.insertBefore(haveFunText, buyTicketButton);
+		buyTicketButton.remove();
+		// OR
+		// buyTicketButton.replaceWith(haveFunText);
+	}
 });
 
 // Log form data
 const contactForm = document.querySelector('.contact__form');
 
 contactForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formObject = Object.fromEntries(formData);
-    console.log(formObject);
+	e.preventDefault();
+	const formData = new FormData(e.target);
+	const formObject = Object.fromEntries(formData);
+	console.log(formObject);
 });
